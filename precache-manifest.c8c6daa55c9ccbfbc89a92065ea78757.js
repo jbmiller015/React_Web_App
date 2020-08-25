7f62ead7a9fba6c1ff436705befe5cf2bf18adba
jbmiller015 <jbmiller015@gmail.com>
2020-08-24 17:09:21 -0700

    Updates

---
+++ precache-manifest.c8c6daa55c9ccbfbc89a92065ea78757.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854e2ee112e9007f1e0c38ecac9ef95d",
+    "url": "/React_Web_App/index.html"
+  },
+  {
+    "revision": "1eada1dd98e8334b3a85",
+    "url": "/React_Web_App/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a78f2512f9364c06034d",
+    "url": "/React_Web_App/static/css/main.8d8996ea.chunk.css"
+  },
+  {
+    "revision": "1eada1dd98e8334b3a85",
+    "url": "/React_Web_App/static/js/2.b6808754.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React_Web_App/static/js/2.b6808754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a78f2512f9364c06034d",
+    "url": "/React_Web_App/static/js/main.590a4131.chunk.js"
+  },
+  {
+    "revision": "88ae6dd0b7e216c6df81",
+    "url": "/React_Web_App/static/js/runtime-main.6c34b922.js"
+  },
+  {
+    "revision": "a93a227b03c4a861ecd44fa2c1d589f7",
+    "url": "/React_Web_App/static/media/comingSoon.a93a227b.jpg"
+  }
+]);
